25215f3b0ba061a530adf4046f0a24e0def4022c
Alex Davyskiba <zviryatko@html-and-cms.com>
2016-02-09 14:08:18 +0200

    Migration to qt

---
+++ imguploader.cpp
@@ -0,0 +1,57 @@
+#include "imguploader.h"
+
+imgUploader::imgUploader(QObject *parent) :
+    QObject(parent)
+{
+    UserAgent = "TickMo";
+    Uploading = false;
+}
+
+void imgUploader::uploadImage(QString FileName, QString UploadURL, QString FieldName)
+{
+
+    QUrl serviceUrl = QUrl(UploadURL);
+
+    QHttpMultiPart *multiPart = new QHttpMultiPart(QHttpMultiPart::FormDataType);
+
+    QNetworkRequest request(serviceUrl);
+    request.setHeader(QNetworkRequest::UserAgentHeader, UserAgent);
+
+    QFile *file = new QFile(FileName);
+    QFileInfo *fileInfo = new QFileInfo(FileName);
+    file->setParent(multiPart);
+    file->open(QIODevice::ReadOnly);
+
+    QHttpPart imagePart;
+    imagePart.setHeader(QNetworkRequest::ContentTypeHeader, QVariant("image/png"));
+    imagePart.setHeader(QNetworkRequest::ContentDispositionHeader, QVariant("form-data; name=\"" + FieldName + "\"; filename=\"" + fileInfo->fileName() +  "\""));
+
+    imagePart.setBodyDevice(file);
+    multiPart->append(imagePart);
+
+    request.setHeader(QNetworkRequest::ContentTypeHeader, "multipart/form-data; boundary=" + multiPart->boundary());
+    currentUpload =  manager.post(request, multiPart);
+    multiPart->setParent(currentUpload);
+
+    connect(currentUpload,SIGNAL(uploadProgress(qint64,qint64)),this, SLOT(onUploadProgress(qint64,qint64)));
+    connect(currentUpload, SIGNAL(finished()),this,SLOT(onUploadFinished()));
+
+    Uploading = true;
+}
+
+bool imgUploader::isUploading()
+{
+    return Uploading;
+}
+
+void imgUploader::onUploadFinished()
+{
+    Uploading = false;
+    currentUpload->deleteLater();
+    emit uploadFinished();
+}
+
+void imgUploader::onUploadProgress(qint64 uploaded, qint64 total)
+{
+    emit uploadProgress( uploaded,  total);
+}
---
+++ settingsdialog.h
@@ -0,0 +1,27 @@
+#ifndef SETTINGSDIALOG_H
+#define SETTINGSDIALOG_H
+
+#include <QDialog>
+#include <QSettings>
+
+namespace Ui {
+class SettingsDialog;
+}
+
+class SettingsDialog : public QDialog
+{
+    Q_OBJECT
+
+public:
+    explicit SettingsDialog(QSettings *parentSettings, QWidget *parent = 0, Qt::WindowFlags f = 0);
+    ~SettingsDialog();
+
+private slots:
+    void on_loginButton_clicked();
+
+private:
+    Ui::SettingsDialog *ui;
+    QSettings *settings;
+};
+
+#endif // SETTINGSDIALOG_H
